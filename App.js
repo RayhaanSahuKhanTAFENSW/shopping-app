91c168bcf65128fe0f54bb62fb82a94c6f440f09
RayhaanSahuKhanTAFENSW <rayhaan.sahukhan@studytafensw.edu.au>
2024-11-04 12:36:04 +1100

    added themes

--- App.js
+++ App.js
@@ -4,18 +4,29 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './navigation/RootNavigator';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { ThemeProvider, useTheme } from './components/ThemeContext';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ThemeProvider>
+        <Main />
+      </ThemeProvider>
       <StatusBar style="auto" />
     </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
+function Main() {
+  const { theme } = useTheme();
 
-});
+  return (
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+      <StatusBar style='auto' />
+    </PaperProvider>
+  );
+}
