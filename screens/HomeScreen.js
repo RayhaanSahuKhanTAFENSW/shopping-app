c0f085a019082842677fc65b455ecfbb70225c7b
RayhaanSahuKhanTAFENSW <rayhaan.sahukhan@studytafensw.edu.au>
2024-11-04 13:09:00 +1100

    added api functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Button, StyleSheet } from 'react-native'
-import { Surface, Text} from "react-native-paper";
-
+import { Surface, Text } from 'react-native-paper';
+import { fetchAllCategories } from '../utils/api';
+ 
 export default function HomeScreen(props) {
+ 
+  const [categories, setCategories] = useState([]);
+ 
+  useEffect(() => {
+   
+    const fetchData = async () => {
+      const fetchCategories = await fetchAllCategories();
+      setCategories(fetchCategories);
+    };
+ 
+    fetchData();
+     
+  }, []);
+ 
   return (
-    <Surface style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+    <Surface style={styles.container} mode='flat' elevation={0}>
+      <Text variant='displaySmall'>Home Screen</Text>
+      {
+        categories.map((category) => (
+          <Text key={category.id}>{category.name}</Text>
+        ))
+      }
     </Surface>
   )
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    fontSize: 18,
-    marginBottom: 20,
-  },
 });
+ 
